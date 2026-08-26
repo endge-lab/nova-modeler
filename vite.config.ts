81ed33da967bb907479f39c2cc7c4a755d83b2e6
Egor Kozelskij <ekozelskiy@integrotechnologies.ru>
2026-08-26 10:30:52 +0300

    feat: ts update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'vitest/config'
-import dts from 'vite-plugin-dts'
+import dts from 'unplugin-dts/vite'
 
 const externalPackages = [
   '@endge/nova',
@@ -24,7 +24,7 @@ export default defineConfig({
       external: isExternal,
     },
   },
-  plugins: [dts({ rollupTypes: true, tsconfigPath: './tsconfig.app.json' })],
+  plugins: [dts({ bundleTypes: false, tsconfigPath: './tsconfig.app.json' })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
